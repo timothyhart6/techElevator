2da853b71843fddd28712e61f2d68cb4c40e607b
Brian Lauvray <lauvray.1@osu.edu>
2018-07-23 19:39:49 -0400

    Added Intro to JQuery

---
+++ m4-w10d2-js-intro-to-jquery-lecture/script.js
@@ -0,0 +1,124 @@
+﻿//1. Write a function that finds box1 
+//      and adds the class "grow"
+function growBox1() {
+    //var box1 = $("#box1");
+    //box1.addClass("grow");
+    $("#box1").addClass("grow");
+
+}
+
+//2. Write a function that finds and
+//      shrinks the blue box
+function shrinkBlueBox() {
+    $(".blue").addClass("shrink");
+}
+
+
+//3. Remove all of the elements inside of a div on the page
+function removeAllStuffs() {
+    $("div").empty(); 
+}
+
+//4. Detach all of the divs on the page
+var boxes;
+function detachDivs() {
+    boxes = $("div").detach();
+
+}
+
+//5. Reattach the divs on the page
+function reattachDivs() {
+    $("section").append(boxes);
+}
+
+//6. Change the text for all of the <p> tags inside boxes
+//      to say a message
+function changeText(message) {
+    $(".box p").text(message);
+}
+
+//7. Change the li to have <b>List Item</b> as their text
+function makeBold() {
+    $("li").text("<b>List Item</b>");
+    //$("li").css("font-weight", "bold");
+}
+
+//8. Add the .shadow class to the last box
+function addShadowToLastBox1() {
+    $(".box").last().addClass("shadow");
+}
+
+function addShadowToLAstBox2(number) {
+    $(`.box:nth-last-of-type(${number})`).addClass("shadow");
+}
+
+
+//9. Toggle the .shadow class on any box passed in
+function toggleShadow(selector) {
+    $(selector).toggleClass("shadow");
+}
+
+
+//10. Make any input textbox have the .yellow class
+//      to give it a yellow background
+function makeYellowTextboxes() {
+    $("input[type='text']").addClass("yellow");
+}
+
+
+//11. Change the value of the textbox to "Hello World"
+function setTextboxValue(msg) {
+    $("input[type='text']").val(msg);
+}
+
+//12. Now go back and allow a message to be passed in
+
+//13. Append a new list item
+function addNewListItem() {
+    //Find out how many li we currently have
+    var count = $("#box3 ul").children().length;
+
+    //Create a new list item
+    var li = $("<li>").text(`Item ${count + 1}`);
+
+    //Find the list (<ul>) that we will add the list item to
+    var ul = $("#box3 ul");
+
+    //Append our new list item
+    ul.append(li);
+}
+
+
+
+//14. Add a new checkbox and textbox
+//      - make sure to change the id
+function addNewItem() {
+    //Find the item we want to copy
+    var htmlToCopy = $("#templateItem")[0].innerHTML;
+
+    var newDiv = $(htmlToCopy);
+
+    //Get the container we want to add to
+    var box6 = $("#box6");
+
+    //Append the copied div
+    box6.append(newDiv);
+}
+
+//15. Now make it possible to switch a textbox to a label
+function switchTextboxToLabel(){
+    $( "input[type=text]" ).replaceWith( function() {
+        return "<label class='newLabel'>" + $( this ).val() + "</label>";
+    });
+}
+
+function switchTextLabelToTextBox(){
+    $( ".newLabel" ).replaceWith( function() {
+        return "<input type=\"text\" value=\"" + $( this ).html() + "\" />";
+    });
+}
+
+//16. Make all the boxes fade out
+function disappear() {
+    $(".box").fadeOut(10000);
+}
---
+++ m4-w10d2-js-intro-to-jquery-lecture/vanilla-script.js
@@ -0,0 +1,86 @@
+﻿document.addEventListener("DOMContentLoaded", function() {
+
+    //1. Getting an HTML element by id
+    var box1 = document.getElementById('box1');    
+    console.log(box1);
+        
+    //2. Getting an HTML element by class name
+    var redBoxes = document.getElementsByClassName('red');
+    console.log(redBoxes);
+
+    //3. Getting HTML elements by tag name
+    var divBoxes = document.getElementsByTagName('div');
+    console.log(divBoxes);   
+
+    //4. Filtering the div boxes to include only odd-numbered boxes
+    var oddNumberedBoxes = document.querySelectorAll('div:nth-child(odd)');
+    console.log(oddNumberedBoxes);
+
+    //5. Getting all list Items and filtering to only include special
+    var specialItems = document.querySelectorAll("li.special");
+    console.log(specialItems);
+      
+    //6. Getting the last box
+    var h2s = document.querySelectorAll("div.box");
+    console.log(h2s[h2s.length - 1]);
+
+    //7. Getting the parent box of the .text class
+    var parent = document.querySelector(".text").parentNode;
+    console.log(parent);
+
+    //8. Getting the box after the blue-one
+    var boxAfterList = document.querySelector("div.blue").nextElementSibling;
+    console.log(boxAfterList);
+
+    //9. Getting all children inside the grey box except <br/>
+    var greyBoxChildren = Array.from(document.getElementsByClassName("grey")[0].children).filter(function(el){return el.tagName !== 'BR'});
+    console.log(greyBoxChildren);
+
+    //10. Getting the value of the text box
+    var textBoxValue = document.getElementById("nameField").value;
+    console.log(textBoxValue);
+
+    //11. Setting the value of the text box to Tech Elevator
+    document.getElementById("nameField").value = "Tech Elevator Again";
+    
+    //12. Getting the value of the first <p> tag in the green box
+    var greenBoxChildren = document.getElementsByClassName('green')[0].children;
+    var greenBoxPTag = Array.from(greenBoxChildren).filter(function(el){return el.tagName === 'P'});    
+    console.log("Before " + greenBoxPTag[0].innerHTML);
+
+    //13. Changing the html
+    greenBoxPTag[0].innerHTML = "<b>This HTML was changed by JavaScript</b>";
+
+    //14. Changing all of the li tags to something better
+    var allLis = document.getElementsByTagName("li");
+    for(var i = 0; i < allLis.length; i++) {
+        allLis[i].textContent = "Set the .text() through vanilla JS";
+    }
+
+    //15. Changing the link attribute in the green box
+    var greenBoxLink = document.querySelector('.green a');
+    greenBoxLink.href = "http://www.techelevator.com";
+
+    //16. Changing the boxes in the grey box    
+    var checkboxes = document.querySelectorAll(".grey input[type=checkbox]");
+    for(var i = 0; i < checkboxes.length; i++) {
+        checkboxes[i].checked = true;
+    }
+
+    //17. Making all of the other boxes grey and the black box red
+    //var blackBox = $(".grey");
+    //var otherBoxes = $(".box").not(".grey");        
+    //blackBox.removeClass("grey").addClass("red");
+    //otherBoxes.removeClass().addClass("box").addClass("black");
+    
+    //18. Adding a new list item
+    var newChildElement = document.createElement("li"); 
+    newChildElement.textContent = "Item 6";
+    document.getElementsByTagName("ul")[0].appendChild(newChildElement);
+
+    //19. Moving the even numbered items from the yellow box to the green box
+    var list = $("li").filter(":even").detach();
+    $(".green").children().filter("ol").append(list);
+    
+
+});
